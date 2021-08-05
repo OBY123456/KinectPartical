f2bfeeb31619a6a14e37f83fcea319af4bad562a
oubaoyao <673411498@qq.com>
2021-08-05 11:09:25 +0800

    把视频外置出去了

--- Assets/OBY/PersonControl.cs
+++ Assets/OBY/PersonControl.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class PersonControl : MonoBehaviour
 {
 
     public KinectManager kinectManager;
     public GameObject[] PersonGroup;
-
+    public VideoPlayer videoPlayer;
     private void Awake()
     {
         kinectManager.LostUser += UserLost;
@@ -16,7 +17,8 @@ public class PersonControl : MonoBehaviour
 
     private void Start()
     {
-        
+        videoPlayer.url = Application.streamingAssetsPath + "/bg.mp4";
+        videoPlayer.Play();
     }
 
     public void UserDetected(int userIndex)
